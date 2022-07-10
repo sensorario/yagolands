8f995a8b814cbdb8e5017e9283cca22dd9f03fa9
Simone Gentili <sensorario@gmail.com>
2022-07-10 21:40:58 +0200

    feat(queue): accept only messages with type

--- src/queue/queue.js
+++ src/queue/queue.js
@@ -1,14 +1,22 @@
+// @todo order items per scheduling time
+
 class Queue {
 
     constructor() {
-
+        this.items = [];
     }
 
     isEmpty() {
-        return true;
+        return this.items.length === 0;
     }
 
-    send() {
+    send(message) {
+        // @todo extract validator for a message
+        if (typeof message.type === 'undefined') {
+            throw 'message type is missing';
+        }
+
+        this.items.push(message);
     }
 
 }
--- src/queue/queue.test.js
+++ src/queue/queue.test.js
@@ -5,10 +5,15 @@ test('queue starts empty', () => {
     expect(queue.isEmpty()).toBe(true);
 })
 
+test('queue message must contains type of message', () => {
+    const wrongMessage = {};
+    expect(() => queue.send(wrongMessage)).toThrow('message type is missing');
+})
+
 test('queue receive a message with instructions', () => {
     const message = {
-        building: 'castle',
+        type: 'building_instructions',
     };
     queue.send(message);
-    expect(queue.isEmpty()).toBe(true);
+    expect(queue.isEmpty()).toEqual(false);
 })
