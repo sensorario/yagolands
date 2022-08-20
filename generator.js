41541c03e88964ee7fc65a7ce4036835cfbd2a06
Simone Gentili <sensorario@gmail.com>
2022-08-21 00:11:45 +0200

    feat: add comment

--- generator.js
+++ generator.js
@@ -1,3 +1,5 @@
+// this code is duplicated in client and server, ...
+
 exports.generateID = function () {
     function s4() {
         return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
